71eb3b6cb9cadc7eeb7a7f2c8de14e5824c5e0b6
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2024-04-08 12:01:25 -0500

    feat: :sparkles: The objective of the first backend route was met.

--- app/controllers/earthquakes_controller.rb
+++ app/controllers/earthquakes_controller.rb
@@ -1,10 +1,89 @@
+require 'net/http'
+require 'json'
+
 class EarthquakesController < ApplicationController
   before_action :set_earthquake, only: %i[ show edit update destroy ]
 
   # GET /earthquakes or /earthquakes.json
   def index
     @earthquakes = Earthquake.all
+
+    if params[:page] && params[:page].to_i > 1
+      puts params[:page]
+
+      totalReg = 10*params[:page].to_i
+
+      if @earthquakes.count >= totalReg
+
+        render :index
+      else
+        base_url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=NOW%20-%2030%20days'
+        url = "#{base_url}&limit=#{totalReg}"
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        data = JSON.parse(response)
+        test_features = data['features']
+        actual = @earthquakes.count
+
+        test_features.drop(actual).each do |item|
+          puts item['properties']['title']
+
+          if item['properties']['tsunami'] != 0
+            item['properties']['tsunami'] = true
+          else
+            item['properties']['tsunami'] = false
+          end
+
+          Earthquake.create(
+            title: item['properties']['title'],
+            mag_type: item['properties']['magType'],
+            tsunami: item['properties']['tsunami'],
+            time: item['properties']['time'].to_s,
+            place: item['properties']['place'],
+            magnitude: item['properties']['mag'],
+            externa_id: item['id'],
+            type: item['properties']['type'],
+            latitude: item['geometry']['coordinates'][1],
+            longitude: item['geometry']['coordinates'][0]
+          )
+        end
+
+      end
+
+    end
+
+    if @earthquakes.count >= 10
+
+      render :index
+    else
+      uri = URI('https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&limit=10&starttime=NOW%20-%2030%20days')
+      response = Net::HTTP.get(uri)
+      data = JSON.parse(response)
+      test_features = data['features']
+      #first_fea = data['features'][0]
+
+      #puts first_fea.inspect
+
+      #puts first_fea['properties']['title']
+
+      test_features.each do |item|
+        puts item['properties']['title']
+
+        if item['properties']['tsunami'] != 0
+          item['properties']['tsunami'] = true
+        else
+          item['properties']['tsunami'] = false
+        end
+
+        Earthquake.create(title: item['properties']['title'],mag_type:item['properties']['magType'],tsunami:item['properties']['tsunami'],time:item['properties']['time'].to_s,place:item['properties']['place'],magnitude:item['properties']['mag'],externa_id:item['id'],type:item['properties']['type'],latitude:item['geometry']['coordinates'][1],longitude:item['geometry']['coordinates'][0] )
+      end
+
+    end
+
+
   end
+
+
 
   # GET /earthquakes/1 or /earthquakes/1.json
   def show
--- app/views/earthquakes/_earthquake.json.jbuilder
+++ app/views/earthquakes/_earthquake.json.jbuilder
@@ -1,2 +1,30 @@
-json.extract! earthquake, :id, :latitude, :longitude, :title, :mag_type, :tsunami, :time, :place, :magnitude, :externa_id, :type, :created_at, :updated_at
-json.url earthquake_url(earthquake, format: :json)
+json.data do
+  @earthquakes.each do |earthquake|
+    json.child! do
+      json.id earthquake.id
+      json.type 'feature'
+      json.attributes do
+        json.external_id earthquake.externa_id
+        json.magnitude earthquake.magnitude
+        json.place earthquake.place
+        json.time earthquake.time.to_s
+        json.tsunami earthquake.tsunami
+        json.mag_type earthquake.mag_type
+        json.title earthquake.title
+        json.coordinates do
+          json.longitude earthquake.longitude
+          json.latitude earthquake.latitude
+        end
+      end
+      json.links do
+        json.external_url earthquake_url(earthquake, format: :json)
+      end
+    end
+  end
+end
+
+json.pagination do
+  json.current_page 1
+  json.total @earthquakes.count
+  json.per_page 10
+end
