301af18ac5f3828ff0b8f14452e537b452d05f19
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2024-04-26 23:30:33 -0500

    fix: :bug: :)

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Backend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.hosts << "https://ruby-testfrogmii.onrender.com"
 
 
     # Configuración CORS para permitir todos los orígenes
